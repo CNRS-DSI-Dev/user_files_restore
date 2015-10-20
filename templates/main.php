5b235e739f586029e0279e2913eff5a1e85788c7
Patrick Paysant <patrick.paysant@linagora.com>
2015-10-20 15:24:24 +0200

    Redesign the requests list page.

--- templates/main.php
+++ templates/main.php
@@ -18,8 +18,19 @@
 
 <div id="container" class="user_files_restore">
 
-<div id="freeCreate" class="dataBlock">
-    <p class="header"><?php p($l->t('Request a complete restoration')); ?> <img src="<?php print_unescaped(image_path('user_files_restore', 'help.png')) ?>" title="<?php print_unescaped($l->t("You also can request a restoration on the application default page.")); ?>" /></p>
+<div id="infos" class="dataBlock">
+    <h2>Informations préalables</h2>
+    <div id="infos__detail">
+        <ul>
+            <li class="freeCreate"><?php print_unescaped($l->t("You also can request a restoration on the application default page.")); ?></li>
+            <li class="running"><?php print_unescaped($l->t('Restoration requests are processed as background jobs. You can find on this page the state and results of your requests.<br /><b>Important</b>: restored files will overide (ie delete and replace) existing files with same path and name.')); ?></li>
+            <li id="crypto"><?php print_unescaped($l->t("If you have been targeted by a crypto-virus, please ask your SSI before creating any restoration request.")); ?></li>
+        </ul>
+    </div>
+</div>
+
+<div id="freeCreate" class="dataBlock clearfix">
+    <p class="header"><?php p($l->t('Request a complete restoration')); ?> <img src="<?php print_unescaped(image_path('user_files_restore', 'help.png')) ?>" /></p>
     <div>
         <select name="version">
             <?php foreach ($_['versions'] as $version): ?>
@@ -30,11 +41,11 @@
     </div>
 </div>
 
-<div id="todo" class="dataBlock">
+<div id="todo" class="dataBlock clearfix">
     <p class="header"><?php p($l->t('To be processed')); ?></p>
     <?php foreach($_['todos'] as $todo): ?>
     <p id="<?php p($todo['id']); ?>">
-        <span title="<?php p($todo['complete_filename']); ?>"><?php p($todo['file']); ?><span>
+        <span title="<?php p($todo['complete_filename']); ?>"><?php p($todo['file']); ?></span>
         <span class="bonus">(<?php p($todo['mime']); ?> ; </span>
         <span class="bonus"><?php p($l->t('d - ') . $todo['version']); ?>)</span>
         <span class="cancel bonus" data-id="<?php p($todo['id']); ?>" data-version="<?php p($todo['version']); ?>"><?php p($l->t('Cancel')); ?></span>
@@ -45,7 +56,7 @@
 <div id="running" class="dataBlock">
     <p class="header">
         <?php p($l->t('Running')); ?>
-        <img src="<?php print_unescaped(image_path('user_files_restore', 'help.png')) ?>" title='<?php print_unescaped($l->t('Requests are not processed in real time. Please refer to the documentation.')); ?>' />
+        <img src="<?php print_unescaped(image_path('user_files_restore', 'help.png')) ?>" />
         <?php if ($_['precedingRequests'] > 0): ?>
         <span><?php p($l->t("You have %d request(s) before yours to be processed.", $_['precedingRequests'])) ;?></span>
         <?php endif ?>
@@ -59,17 +70,25 @@
 </div>
 
 <div id="done" class="dataBlock">
-    <p class="header"><?php p($l->t('Done')); ?></span></p>
-    <?php foreach($_['dones'] as $done): ?>
-    <p>
+    <p class="header"><?php p($l->t('Done')); ?></p>
+    <?php
+        foreach($_['dones'] as $done):
+            $classDone = "";
+            if (!empty($done['error'])) {
+                $classDone = "error";
+            }
+    ?>
+    <p class="<?php  echo $classDone ?>">
         <span class="bonus">(<?php p($done['mime']); ?>)</span>
         <span title="<?php p($done['complete_filename']); ?>"><?php p($done['file']); ?></span>
         <span class="errorback">
             <?php if (!empty($done['error'])): ?>
-            <img src="<?php print_unescaped(image_path('user_files_restore', 'exclamation.png')) ?>" title="<?php print_unescaped($done['error']); ?>" /></span>
+            <img src="<?php print_unescaped(image_path('user_files_restore', 'exclamation.png')) ?>" />
+            <span  title="<?php print_unescaped($done['error']); ?>">Voir plus</span>
             <?php else: ?>
-            &nbsp;</span>
+            &nbsp;
             <?php endif; ?>
+            </span>
         <span class="date bonus"><?php p($done['dateEnd']); ?></span>
     </p>
     <?php endforeach; ?>
